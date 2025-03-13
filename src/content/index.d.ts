b08002c536231dc3526cbfafa860eba5e8599859
Hung Nguyen <nthungdev@gmail.com>
2025-03-13 15:15:49 -0400

    add content json

--- src/content/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.md" {
-  import React from "react";
-  const html: string;
-  const body: string;
-  const meta: { resourcePath: string };
-  const attributes: Record<string, unknown>;
-  const react: React.VFC;
-  export { attributes, html, meta, body, react };
-}
