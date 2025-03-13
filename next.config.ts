86f165862642c4457b5a5f9bf93b9ccda0b7a196
Hung Nguyen <nthungdev@gmail.com>
2025-03-12 21:30:15 -0400

    add loader for markdown

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: {
+        mode: ["react-component", "html", "body", "meta"],
+      },
+    });
+
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.md": {
+          as: "*.js",
+          loaders: [
+            {
+              loader: "frontmatter-markdown-loader",
+              options: {
+                mode: ["react-component", "html", "body", "meta"],
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
