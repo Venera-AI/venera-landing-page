409eaf692d8fe791016e338dd571bebfa6da5654
Huong Pham <hazel@viai.io>
2025-03-18 15:16:43 -0400

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import FeedbackSection from "@/app/sections/feedback-section";
-
+import IntroSection from "@/app/sections/intro-section";
 export default function Home() {
   return (
     <main>
       <h1 className="sr-only">Home</h1>
+      <IntroSection />
       <FeedbackSection />
     </main>
   );
