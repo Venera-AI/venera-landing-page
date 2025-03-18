7a7c6bd0ea9378a2e900f312201d9463a566f46f
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2025-03-17 21:00:27 -0400

    feedback section (#8)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import FeedbackSection from "@/app/sections/feedback-section";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <main>
+      <h1 className="sr-only">Home</h1>
+      <FeedbackSection />
+    </main>
+  );
 }
