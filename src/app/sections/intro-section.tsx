6f83765ef48375fb64886fae22f2c89452c1f736
Huong Pham <hazel@viai.io>
2025-03-20 19:26:19 -0400

    update

--- src/app/sections/intro-section.tsx
+++ src/app/sections/intro-section.tsx
@@ -2,23 +2,20 @@ import React from "react";
 
 export default function IntroSection() {
   return (
-    <div className="w-full h-screen bg-gray-100 flex flex-col items-center mb-10">
+    <div className="w-full h-screen bg-gray-100 flex flex-col items-center md:mb-10 mb-10">
       <nav className="w-full md:min-h-[90px] h-[140px] flex md:flex-nowrap justify-between items-center px-4 sm:px-6 py-3 bg-gradient-to-r from-[#83a3cb] to-[#5162b2] shadow-md">
-        {/* Logo Section */}
         <div className="flex justify-center md:justify-start md:w-auto">
           <img
             src="assets/venera-logo.svg"
-            className="w-[100px] sm:w-[120px] md:w-auto"
+            className="min-w-[160px] min-h-[70px] md:w-auto"
             alt="Venera Logo"
           />
         </div>
-
-        {/* Buttons Section */}
-        <div className="flex md:flex-nowrap md:w-full md:gap-4 mt-2 md:mt-0 w-full md:w-auto justify-end">
-          <button className="md:w-[160px] mr-[10px] md:h-[55px] w-1/3 px-4 sm:px-5 py-2 sm:py-2.5 border border-white text-white bg-transparent rounded-md hover:bg-white hover:text-black text-sm">
+        <div className="flex md:flex-nowrap md:w-full md:gap-4 mt-2 md:mt-0 w-full justify-end">
+          <button className="md:w-[160px] mr-[10px] md:h-[55px] md:text-base w-1/3 min-h-3 px-4 sm:px-5 py-2 sm:py-2.5 border border-white text-white bg-transparent rounded-md hover:bg-white hover:text-black">
             Log in
           </button>
-          <button className="md:w-[160px] md:h-[55px] w-1/3 px-4 sm:px-5 py-2 sm:py-2.5 text-[#2d44ad] bg-white rounded-md hover:bg-[#2d44ad] hover:text-white text-sm">
+          <button className="md:w-[160px] md:h-[55px] md:text-base w-1/3 px-4 sm:px-5 py-2 sm:py-2.5 text-[#2d44ad] bg-white rounded-md hover:bg-[#2d44ad] hover:text-white">
             Try for free
           </button>
         </div>
@@ -45,27 +42,27 @@ export default function IntroSection() {
           <div className="absolute w-full flex justify-center items-center">
             <img
               src="/assets/phone-effect.png"
-              className="w-[260px] md:w-[537.6px] h-auto"
+              className="w-[300px] md:w-[537.6px] h-auto"
               alt="Phone Effect"
             />
           </div>
-          <div className="relative z-10 flex flex-col items-center">
+          <div className="relative z-10 flex flex-col items-center ">
             <img
               alt="phone mock up"
               src="/assets/phone-screen.png"
-              className="max-w-[250px] sm:max-w-[1080px]"
+              className="md:max-w-[1100px] max-w-[600px]"
             />
           </div>
-          <div className="absolute z-20 top-[30%] md:top-[60%] left-[35%] transform translate-x-[-50%] mt-6 gap-4 px-3 sm:px-0">
-            <button className="w-[100px] md:min-w-[220px] h-14 px-6 py-4 bg-white text-black rounded-lg flex flex-row items-center gap-3 hover:bg-gray-200 mb-2">
+          <div className="absolute z-20 top-[30%] md:top-[60%] left-[35%] transform translate-x-[-50%] mt-6 gap-4 px-3">
+            <button className="w-[100px] md:min-w-[220px] h-14 px-6 py-4 bg-white text-black rounded-lg flex flex-row items-center hover:bg-gray-200 mb-2">
               <img
                 src="/assets/apple-logo.svg"
                 alt="button App store"
-                className="w-6 sm:w-8 h- md:h-8"
+                className="w-3 sm:w-8 h-3 md:h-8"
               />
-              <div className="flex flex-col leading-tight">
+              <div className="flex flex-col leading-tight text-xs">
                 <span className="text-sm md:text-base">Get it on</span>
-                <b className="text-lg md:text-xl">App Store</b>
+                <div className="text-xs md:text-xl font-bold">App Store</div>
               </div>
             </button>
           </div>
