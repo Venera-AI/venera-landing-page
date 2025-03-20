b874fc5beb0272081a2a0c58cd655bc585671ca9
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-20 08:22:25 +0700

    style: handle why section viewport breaks

--- src/app/sections/why-section.tsx
+++ src/app/sections/why-section.tsx
@@ -3,21 +3,33 @@ import Image from "next/image";
 
 export default function WhySection() {
   return (
-    <section className="flex flex-col md:flex-row items-center justify-center py-16 px-8 md:p-16 bg-white">
-      <h2 className="text-4xl">{why.headline}</h2>
-      {why.items.map((item, index) => (
-        <div key={index} className="flex flex-col items-center">
-          <Image
-            className=""
-            src={item.image}
-            alt={`${item.headline} image`}
-            width={100}
-            height={100}
-          />
-          <h3>{item.headline}</h3>
-          <p>{item.body}</p>
-        </div>
-      ))}
+    <section className="flex flex-col items-center justify-between py-16 px-8 md:p-16 bg-white">
+      <h2 className="text-4xl font-medium">{why.headline}</h2>
+      <div className="flex flex-col xl:flex-row justify-around">
+        {why.items.map((item, index) => (
+          <div key={index} className="flex flex-col items-center p-8">
+            <Image
+              className="size-76"
+              src={item.image}
+              alt={`${item.headline} image`}
+              width={100}
+              height={100}
+            />
+            <div className="flex gap-x-6">
+              <div className="h-10 w-4 rounded-full bg-navyblue" />
+              <div>
+                <h3 className="text-navyblue font-bold text-xl">
+                  {item.headline}
+                </h3>
+                <p className="mt-4">{item.body}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="bg-navyblue text-white px-16 py-5 text-xl rounded-2xl font-bold">
+        Meet our team
+      </button>
     </section>
   );
 }
