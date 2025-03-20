0918b645a294ea044c0dcbf6acef863c6632c018
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-20 10:56:41 +0700

    style: handle usage section breakpoints

--- src/app/sections/usage-section.tsx
+++ src/app/sections/usage-section.tsx
@@ -3,10 +3,14 @@ import Image from "next/image";
 
 export default function UsageSection() {
   return (
-    <section className="flex md:flex-row flex-col justify-center items-center py-16 px-8 md:p-16 bg-white">
-      <div className="flex flex-col">
-        <h2 className="text-4xl font-bold text-">{usage.headline}</h2>
-        <p>{usage.body}</p>
+    <section className="flex lg:flex-row flex-col gap-8 justify-center items-center py-16 px-8 bg-white">
+      <div className="flex flex-col lg:w-1/3">
+        <h2 className="text-4xl font-medium text-center lg:text-start">
+          {usage.headline}
+        </h2>
+        <p className="text-xl mt-8 lg:w-[80%] text-center lg:text-start">
+          {usage.body}
+        </p>
       </div>
       <Image
         src={"/images/usage.webp"}
