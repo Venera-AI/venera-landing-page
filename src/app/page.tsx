9ed511f367e4829c0199545585a75c9004208989
Huong Pham <hazel@viai.io>
2025-03-21 11:51:13 -0400

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import FeedbackSection from "@/app/sections/feedback-section";
-import HeroSection from "@/app/sections/hero-section";
 import FooterSection from "@/app/sections/footer-section";
 import IntroSection from "@/app/sections/intro-section";
 import WhySection from "@/app/sections/why-section";
 import ContactSection from "@/app/sections/contact-section";
+import HeroSection from "@/app/sections/hero-section";
 export default function Home() {
   return (
     <main>
