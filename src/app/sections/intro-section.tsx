48c2548cbe2b845a144f53e560684f5be62a9348
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-22 21:45:12 +0700

    style: set max width for container

--- src/app/sections/intro-section.tsx
+++ src/app/sections/intro-section.tsx
@@ -3,22 +3,24 @@ import Image from "next/image";
 
 export default function IntroSection() {
   return (
-    <section className="flex lg:flex-row flex-col gap-8 justify-center items-center py-16 px-8 bg-white">
-      <div className="flex flex-col lg:w-1/3">
-        <h2 className="text-4xl font-medium text-center lg:text-start">
-          {intro.headline}
-        </h2>
-        <p className="text-xl mt-8 lg:w-[80%] text-center lg:text-start">
-          {intro.body}
-        </p>
+    <section className="bg-white">
+      <div className="flex flex-col lg:flex-row gap-8 items-center justify-center max-w-7xl py-16 px-8 m-auto">
+        <div className="flex flex-col lg:w-1/3">
+          <h2 className="text-4xl font-medium text-center lg:text-start leading-12">
+            {intro.headline}
+          </h2>
+          <p className="text-xl mt-8 lg:w-[80%] text-center lg:text-start leading-8">
+            {intro.body}
+          </p>
+        </div>
+        <Image
+          src={intro.image}
+          alt="usage-image"
+          width={565}
+          height={511}
+          className="max-w-[300px] md:max-w-[450px]"
+        />
       </div>
-      <Image
-        src={"/images/intro.webp"}
-        alt="usage-image"
-        width={565}
-        height={511}
-        className="max-w-[300px] md:max-w-[450px]"
-      />
     </section>
   );
 }
