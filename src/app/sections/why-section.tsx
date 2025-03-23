3a759577bcc5f681ee13319f483cf4626e208bb1
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-23 11:09:25 +0700

    style: match colors to design

--- src/app/sections/why-section.tsx
+++ src/app/sections/why-section.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 
 export default function WhySection() {
   return (
-    <section className="bg-white">
+    <section className="bg-[#ecf5ff]">
       <div className="flex flex-col items-center justify-center py-16 px-8 md:p-16 max-w-7xl m-auto">
-        <h2 className="text-4xl font-medium text-center">{why.headline}</h2>
-        <div className="flex flex-col xl:flex-row justify-around mt-8">
+        <h2 className="text-4xl font-medium text-center text-navyblue">
+          {why.headline}
+        </h2>
+        <div className="flex flex-col xl:flex-row justify-between mt-8">
           {why.items.map((item, index) => (
             <div key={index} className="flex flex-col items-center p-8">
               <div className="relative size-76 max-w-[250px] md:max-w-none">
@@ -20,9 +22,9 @@ export default function WhySection() {
               </div>
 
               <div className="flex gap-x-6 xl:max-w-none md:max-w-2/3">
-                <div className="h-10 w-4 rounded-full bg-navyblue" />
+                <div className="h-10 w-4 rounded-full bg-[#2d44ad]" />
                 <div>
-                  <h3 className="text-navyblue font-bold text-xl">
+                  <h3 className="text-[#2d44ad] font-bold text-xl">
                     {item.headline}
                   </h3>
                   <p className="mt-4">{item.body}</p>
@@ -32,7 +34,7 @@ export default function WhySection() {
           ))}
         </div>
         <Link href={why.actionButton.link}>
-          <button className="bg-navyblue text-white md:px-16 md:py-5 px-12 py-4 md:text-xl rounded-2xl font-bold mt-8">
+          <button className="bg-[#2d44ad] text-white md:px-16 md:py-5 px-12 py-4 md:text-xl rounded-2xl font-bold mt-8">
             {why.actionButton.label}
           </button>
         </Link>
