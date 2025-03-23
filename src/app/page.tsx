8fa2faf7d0a7a53d0944faa3cd8ecbbfc9b5cbee
Hiroreco <61302586+Hiroreco@users.noreply.github.com>
2025-03-23 20:46:21 +0700

    VBW-7 Why Section (#12)
    Co-authored-by: LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import FeedbackSection from "@/app/sections/feedback-section";
 import HeroSection from "@/app/sections/hero-section";
+import WhySection from "@/app/sections/why-section";
+
 export default function Home() {
   return (
     <main>
       <h1 className="sr-only">Home</h1>
       <HeroSection />
       <FeedbackSection />
+      <WhySection />
     </main>
   );
 }
