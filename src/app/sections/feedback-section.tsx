ec20a02b6a0a2a4290aae31baff7cc40194c7f65
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2025-03-23 08:49:59 -0400

    VBW-9 feedback section profile photos (#15)

--- src/app/sections/feedback-section.tsx
+++ src/app/sections/feedback-section.tsx
@@ -1,17 +1,26 @@
-import { feedback } from "@/content/home.json";
+import home from "@/content/home.json";
+import Image from "next/image";
 
 export default function FeedbackSection() {
+  const { feedback } = home;
   return (
     <section className="py-16 px-8 md:p-16 bg-white">
       <h2 className="font-medium text-4xl text-center">{feedback.headline}</h2>
-      <ul className="grid grid-rows-3 lg:grid-rows-none lg:grid-cols-3 gap-8 md:gap-16 max-w-7xl mx-auto py-16 md:py-24">
+      <ul className="grid lg:grid-cols-3 gap-8 md:gap-16 max-w-7xl mx-auto py-16 md:py-24">
         {feedback.items.map((item, index) => (
           <li
             key={index}
             className="bg-lightblue rounded-[40px] px-10 py-8 flex flex-col items-center gap-y-6"
           >
             <div className="flex-1 flex flex-col gap-y-6 items-center">
-              <div className="size-20 rounded-full bg-gray-300"></div>
+              <div className="size-20 bg-gray-300 relative rounded-full overflow-hidden">
+                <Image
+                  src={item.image}
+                  alt={`${item.name} avatar`}
+                  fill
+                  className="object-cover"
+                />
+              </div>
               <p className="text-center text-darkteal text-sm leading-relaxed">
                 {item.body}
               </p>
