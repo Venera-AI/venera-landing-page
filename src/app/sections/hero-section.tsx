50c3f5f1f5ce7eb2ac4aa3f786244cf5d8aae767
Huong Pham <hazel@viai.io>
2025-03-25 16:11:04 -0400

    left screen + responsive styling

--- src/app/sections/hero-section.tsx
+++ src/app/sections/hero-section.tsx
@@ -5,49 +5,50 @@ import NavBar from "../components/NavBar";
 export default function HeroSection() {
   const { hero } = homeData;
   return (
-    <div className="w-full h-screen flex flex-col items-center bg-gray-100">
+    <div className="h-screen flex flex-col items-center bg-gray-100 ">
       <NavBar />
-      <div className="max-w-6xl h-screen flex flex-col items-center justify-between p-4 md:flex-row bg-gradient-to-r from-[#9ec5f7] to-darkblue">
-        <div className="w-full text-center p-4 sm:w-1/2 sm:text-left">
-          <div className="relative mt-4 sm:mt-[1rem] text-[1rem]">
-            <div className="relative ml-4 mb-6">
+      <div className="lg:max-w-7xl h-screen md:flex md:flex-col lg:flex-row md:justify-between items-center bg-gradient-to-r from-[#9ec5f7] to-darkblue">
+        <div className="lg:w-1/2 md:text-center md:p-5 md:w-2/3">
+          <div className="relative text-[1rem]">
+            <div className="relative">
               <Image
                 src="/images/Slide1.png"
                 alt="Slide 1"
-                className="w-full"
+                className="md:w-full sm:w-2/3"
                 width={1920}
                 height={1080}
               />
-              <div className="absolute top-[45%] left-8 transform translate-y-[-50%] p-2 sm:p-0">
-                <div className="text-white font-bold md:text-5xl text-3xl mb-3">
+              <div className="absolute top-[35%] left-10 transform translate-y-[-50%] p-3 sm:p-0">
+                <div className="text-white font-bold md:text-7xl text-3xl mb-7">
                   {hero.headline}
                 </div>
-                <div className="text-white sm:text-4xl text-xl">
+                <div className="text-white sm:text-4xl md:text-3xl text-xl text-left">
                   {hero.subheadline}
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="relative w-full flex justify-center items-center mt-[2rem] md:w-1/2">
-          <div className="absolute w-full flex justify-center items-center">
+        <div className="relative flex justify-center items-center lg:w-1/2 md:w-full">
+          <div className="absolute flex justify-center items-center">
             <Image
               src="/images/phone-effect.png"
               alt="Phone Effect"
-              width={300}
+              className="md:w-[537.6px] w-[300px] h-auto"
+              width={250}
               height={0}
             />
           </div>
           <div className="relative z-10 flex flex-col items-center w-full md:w-full">
             <Image
-              alt="phone mock up"
-              src="images/phone-screen.png"
-              className="md:w-full w-full scale-150"
-              width={300}
+              alt="Phone MockUp"
+              src="/images/phone-screen.png"
+              className="lg:w-full w-full object-contain"
+              width={500}
               height={500}
             />
           </div>
-          <div className="absolute z-20 left-[35%] transform translate-x-[-50%] px-3 gap-4 md:top-[65%] top-[30%] md:min-w-[200px] mt-6">
+          <div className="absolute z-20 transform px-2 md:min-w-[200px]">
             <button className="flex flex-row items-center bg-white text-black rounded-lg hover:bg-gray-200 md:h-16 md:px-8 md:py-6 h-8 px-4 py-2 mb-2 max-w-auto">
               <Image
                 src="/images/apple-logo.svg"
