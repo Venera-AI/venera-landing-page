9635dac956b0e98dbf36b0d5ea9f5ffe3e15f0cb
Hiroreco <61302586+Hiroreco@users.noreply.github.com>
2025-03-25 09:59:14 +0700

    VBW-11 Footer Section (#19)
    Co-authored-by: LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
    
    resolve conflicts
    
    fix and merge
    
    fix & resolve
    
    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { redirect } from "@/i18n/navigation";
-import { defaultLocale } from "@/i18n/routing";
-
-export default function RootPage() {
-  return redirect({
-    href: "/",
-    locale: defaultLocale,
-  });
+import FeedbackSection from "@/app/sections/feedback-section";
+import FooterSection from "@/app/sections/footer-section";
+import IntroSection from "@/app/sections/intro-section";
+import WhySection from "@/app/sections/why-section";
+import ContactSection from "@/app/sections/contact-section";
+import HeroSection from "@/app/sections/hero-section";
+export default function Home() {
+  return (
+    <main>
+      <h1 className="sr-only">Home</h1>
+      <HeroSection />
+      <IntroSection />
+      <WhySection />
+      <FeedbackSection />
+      <ContactSection />
+      <FooterSection />
+    </main>
+  );
 }
