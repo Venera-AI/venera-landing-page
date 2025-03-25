2d588b0c755e9fb8d557ebbf17bfa968c8e6a588
Huong Pham <hazel@viai.io>
2025-03-25 14:57:03 -0400

    split into nav component + css styling nav

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,21 +4,21 @@ export default function NavBar() {
   const { hero } = homeData;
   return (
     <div className="w-full">
-      <nav className="w-full flex justify-between items-center px-4 py-1 shadow-md md:min-h-[90px] md:flex-nowrap h-[140px] bg-gradient-to-r from-[#83a3cb] to-[#5162b2]">
-        <div className="flex md:justify-start justify-center md:w-auto">
+      <nav className="flex w-full lg:p-3 lg:px-8 justify-between items-center md:px-6 py-3 px-3 shadow-md bg-gradient-to-r from-[#83a3cb] to-[#5162b2]">
+        <div className="flex justify-center md:justify-start md:w-auto">
           <Image
             src="/images/venera-logo.svg"
             alt="Venera Logo"
-            width={160}
-            height={70}
-            className="md:w-auto min-w-[160px] min-h-[70px]"
+            width={170}
+            height={80}
+            className="sm:min-w-[180px] max-w-[150px]"
           />
         </div>
-        <div className="flex justify-end w-full mt-2 gap-2 md:flex-nowrap md:w-full md:gap-4 md:mt-0">
-          <button className="text-center border border-white text-white font-semibold bg-transparent rounded-xl hover:bg-white hover:text-black md:w-[160px] md:h-[55px] md:text-base w-1/3 h-10 text-xs px-4 py-2">
+        <div className="flex justify-end gap-2 md:gap-4">
+          <button className="lg:w-full lg:text-base lg:px-14 lg:py-4  md:w-full md:text-sm md:px-10 md:py-2 text-center border border-white text-white font-semibold bg-transparent rounded-xl hover:bg-white hover:text-black text-[10px] whitespace-nowrap px-6 py-2">
             {hero.buttons.buttonLogIn.label}
           </button>
-          <button className="text-center text-[#2d44ad] bg-white font-semibold rounded-xl hover:bg-[#2d44ad] hover:text-white md:w-[160px] md:h-[55px] md:text-base w-1/3 h-10 text-xs px-4 py-2">
+          <button className="lg:w-full lg:text-base lg:px-10 lg:py-4 md:w-full md:text-sm md:px-8 md:py-2 text-center text-darkblue bg-white font-semibold rounded-xl hover:bg-darkblue hover:text-white px-4 py-2 whitespace-nowrap text-[10px]">
             {hero.buttons.buttonTryForFree.label}
           </button>
         </div>
