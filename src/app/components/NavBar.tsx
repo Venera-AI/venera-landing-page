94c714069fc7b27071ab353359482aa559f4dd73
Huong Pham <hazel@viai.io>
2025-03-25 20:11:02 -0400

    fix

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -15,10 +15,10 @@ export default function NavBar() {
           />
         </div>
         <div className="flex justify-end gap-2 md:gap-4">
-          <button className="lg:w-full lg:text-base lg:px-14 lg:py-4  md:w-full md:text-sm md:px-10 md:py-2 text-center border border-white text-white font-semibold bg-transparent rounded-xl hover:bg-white hover:text-black text-[10px] whitespace-nowrap px-6 py-2">
+          <button className="lg:w-full lg:text-base lg:px-14 lg:py-4  md:w-full md:text-base sm:text-sm md:px-10 md:py-2 text-center border border-white text-white font-semibold bg-transparent rounded-xl hover:bg-white hover:text-black text-[10px] whitespace-nowrap px-6 py-2">
             {hero.buttons.buttonLogIn.label}
           </button>
-          <button className="lg:w-full lg:text-base lg:px-10 lg:py-4 md:w-full md:text-sm md:px-8 md:py-2 text-center text-darkblue bg-white font-semibold rounded-xl hover:bg-darkblue hover:text-white px-4 py-2 whitespace-nowrap text-[10px]">
+          <button className="lg:w-full lg:text-base lg:px-10 lg:py-4 md:w-full md:text-base sm:text-sm md:px-8 md:py-2 text-center text-darkblue bg-white font-semibold rounded-xl hover:bg-darkblue hover:text-white px-4 py-2 whitespace-nowrap text-[10px]">
             {hero.buttons.buttonTryForFree.label}
           </button>
         </div>
