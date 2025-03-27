d4713e3821e6a88a8343ca8980dd04680dbdfad4
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-27 07:39:20 +0700

    feat: added a custom slide in component

---
+++ src/components/SlideIn.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { motion, HTMLMotionProps } from "framer-motion";
+import { PropsWithChildren } from "react";
+import React from "react";
+
+type SlideDirection = "up" | "down" | "left" | "right";
+
+interface SlideInProps<T extends keyof HTMLElementTagNameMap = "div"> {
+  as?: T;
+  delay?: number;
+  className?: string;
+  direction?: SlideDirection;
+}
+
+const slideVariants = {
+  up: { opacity: 0, y: 50 },
+  down: { opacity: 0, y: -50 },
+  left: { opacity: 0, x: 50 },
+  right: { opacity: 0, x: -50 },
+};
+
+export default function SlideIn<T extends keyof HTMLElementTagNameMap = "div">({
+  as,
+  children,
+  delay = 0,
+  direction = "up",
+  className = "",
+  ...props
+}: PropsWithChildren<SlideInProps<T> & HTMLMotionProps<T>>) {
+  // This alllows us to use the HTML element as is instead of having to wrap it in a motion.div
+  const Component = motion[as || "div"];
+
+  return (
+    // @ts-expect-error Typescript mismatch, can't figure out how to resolve it, but it works fine so meh
+    <Component
+      initial={slideVariants[direction]}
+      whileInView={{ opacity: 1, x: 0, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, delay }}
+      className={className}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+}
