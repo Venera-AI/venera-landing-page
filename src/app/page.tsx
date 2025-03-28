92b5f34ee6420c5cedda444283b1153213e57de8
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-03-28 14:33:08 +0700

    feat: setting up metadata generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import ContactSection from "@/app/sections/contact-section";
 import FeedbackSection from "@/app/sections/feedback-section";
 import FooterSection from "@/app/sections/footer-section";
 import IntroSection from "@/app/sections/intro-section";
 import WhySection from "@/app/sections/why-section";
-import ContactSection from "@/app/sections/contact-section";
+import metadata from "@/content/metadata.json";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  // TODO: handle returning metadata based on language
+  return metadata;
+}
+
 export default function Home() {
   return (
     <main>
