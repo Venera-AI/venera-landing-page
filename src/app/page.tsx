0d3bd239641890a8d68ee200ee3e1cb274088e12
Anh Luu <anhluu@viai.io>
2025-03-28 13:24:34 -0400

    Added dashboard feature page 1 and page 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import IntroSection from "@/app/sections/intro-section";
 import WhySection from "@/app/sections/why-section";
 import ContactSection from "@/app/sections/contact-section";
 import FeedbackSection from "@/app/sections/feedback-section";
+import DashboardFeature1 from "@/app/sections/dashboard-feature1";
+import DashboardFeature2 from "@/app/sections/dashboard-feature2";
 import PrivacySection from "@/app/sections/privacy-section";
 export default function Home() {
   return (
     <main>
       <h1 className="sr-only">Home</h1>
       <IntroSection />
+      <DashboardFeature1 />
+      <DashboardFeature2 />
       <PrivacySection />
       <WhySection />
       <FeedbackSection />
---
+++ src/app/sections/dashboard-feature1.tsx
@@ -0,0 +1,38 @@
+import home from "@/content/home.json";
+import Image from "next/image";
+
+export default function DashboardFeature1() {
+  const { dashboard_1 } = home;
+  return (
+    <section className="bg-white">
+      <div className="flex flex-col lg:flex-row gap-8 items-center max-w-7xl py-16 px-8 m-auto relative">
+        <div className="flex flex-col">
+          <h2 className="text-4xl font-medium text-center lg:text-start leading-12">
+            {dashboard_1.headline}
+          </h2>
+          <p className="text-xl mt-8 lg:w-[80%] text-center lg:text-start leading-8">
+            {dashboard_1.body}
+          </p>
+        </div>
+        <div className="relative">
+          {/* Pink pastel circle moved to the far right */}
+          <div
+            className="absolute z-0 rounded-full w-[450px] h-[450px]"
+            style={{
+              backgroundColor: "#f4e6d9", // Custom pastel pink
+              top: "-60px",
+              right: "-110px",
+            }}
+          ></div>
+          <Image
+            src={dashboard_1.image}
+            alt="Dashboard Feature 1"
+            width={565}
+            height={511}
+            className="relative z-10 max-w-[300px] md:max-w-[450px]"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
