31270d9f739168f29f2fa8787866a7942c633c18
Hung Nguyen <nthungdev@gmail.com>
2025-04-02 01:26:50 -0400

    healthcare data section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import HealthcareDataSection from "@/app/sections/healthcare-data-section";
 import FeedbackSection from "@/app/sections/feedback-section";
 import FooterSection from "@/app/sections/footer-section";
 import IntroSection from "@/app/sections/intro-section";
 import WhySection from "@/app/sections/why-section";
 import ContactSection from "@/app/sections/contact-section";
 import PrivacySection from "@/app/sections/privacy-section";
+
 export default function Home() {
   return (
     <main>
       <h1 className="sr-only">Home</h1>
+      <HealthcareDataSection />
       <IntroSection />
       <PrivacySection />
       <WhySection />
