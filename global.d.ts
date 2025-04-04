97b780780b03bb4ab043a23a2f69da648ed72928
Hung Nguyen <nthungdev@gmail.com>
2025-04-04 17:47:05 -0400

    add privacy and terms pages

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,8 @@
-import { formats } from "@/i18n/request";
-import { Locale } from "@/i18n/routing";
-import messages from "./content/en.json";
-
-declare module "next-intl" {
-  interface AppConfig {
-    Locale: Locale;
-    Messages: typeof messages;
-    Formats: typeof formats;
-  }
+declare module "*.md" {
+  const react: React.FC;
+  const attributes: Record<string, unknown>;
+  const meta: Record<string, unknown>;
+  const html: string;
+  const body: string;
+  export { react, meta, body, html, attributes };
 }
--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,6 @@ const nextConfig: NextConfig = {
         mode: ["react-component", "html", "body", "meta"],
       },
     });
-
     return config;
   },
   compiler: {
