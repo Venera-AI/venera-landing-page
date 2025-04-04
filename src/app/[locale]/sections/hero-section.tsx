db60d2f50ec5b30a7def334b38c1fc49bfcb1de9
Huong Pham <hazel@viai.io>
2025-04-04 15:40:07 -0400

    add internationalization

--- src/app/[locale]/sections/hero-section.tsx
+++ src/app/[locale]/sections/hero-section.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import homeData from "@/content/home.json";
 import Image from "next/image";
 import NavBar from "@/app/components/nav-bar";
+import { useTranslations } from "next-intl";
 
 export default function HeroSection() {
-  const { hero } = homeData;
+  const t = useTranslations("homePage.hero");
+
   return (
     <div className="bg-gradient-to-r from-[#9ec5f7] to-darkblue">
       <NavBar />
@@ -15,7 +16,7 @@ export default function HeroSection() {
             <div className="w-full sm:top-[-30] md:top-0 md:h-auto md:pl-0 lg:top-0 lg:h-auto top-0 left-1/2">
               <div className="relative mx-auto aspect-square w-full max-w-[500px]">
                 <Image
-                  src={hero.images.textEffect}
+                  src={t("images.textEffect")}
                   alt="Left Section Effect"
                   fill
                   className="object-cover"
@@ -23,11 +24,11 @@ export default function HeroSection() {
               </div>
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full lg:pt-20 z-10 transform">
                 <div className="text-white text-center lg:text-left text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold mb-7">
-                  <h1>{hero.headline}</h1>
+                  <h1>{t("headline")}</h1>
                 </div>
                 <div className="text-white text-center lg:text-left text-xl md:text-2xl lg:text-5xl">
                   <span className="inline-block">
-                    <h3>{hero.subheadline}</h3>
+                    <h3>{t("subheadline")}</h3>
                   </span>
                 </div>
               </div>
@@ -41,7 +42,7 @@ export default function HeroSection() {
             <div className="lg:absolute lg:z-40 w-full lg:top-[70%] lg:left-[0] lg:translate-x-[-60%]">
               <button className="relative block mx-auto items-center max-w-[150px] sm:max-w-[180px] lg:max-w-[200px] w-full mb-5 rounded-lg cursor-pointer">
                 <Image
-                  src={hero.buttons.buttonAppStore.img}
+                  src={t("buttons.buttonAppStore.img")}
                   alt="App Store button"
                   className="object-contain w-full h-auto"
                   width={0}
@@ -52,7 +53,7 @@ export default function HeroSection() {
             </div>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full lg:w-[150%] h-full z-10 transform">
               <Image
-                src={hero.images.phoneEffect}
+                src={t("images.phoneEffect")}
                 alt="Phone Effect"
                 className="object-contain"
                 fill
@@ -60,7 +61,7 @@ export default function HeroSection() {
             </div>
             <Image
               alt="Phone MockUp"
-              src={hero.images.mockUpPhone}
+              src={t("images.mockUpPhone")}
               className="relative max-w-[200px] sm:max-w-[240px] md:max-w-[280px] lg:max-w-[300px] w-full z-30"
               width={0}
               height={0}
--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,25 +1,27 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
-import homeData from "@/content/home.json";
 
 export default function NavBar() {
-  const { hero } = homeData;
+  const t = useTranslations("homePage.hero");
+
   return (
     <nav className="flex justify-between items-center w-full py-3 px-3 md:px-6 lg:p-3 lg:px-8 shadow-md bg-gradient-to-r from-[#83a3cb] to-[#5162b2]">
       <div className="flex justify-center md:justify-start">
         <Image
-          src={hero.logos.logoNavBar}
+          src={t("logos.logoNavBar")}
           alt="Venera Logo"
           width={200}
           height={80}
           className="w-full max-w-full h-auto sm:min-w-[200px]"
         />
       </div>
+
       {/* <div className="flex justify-end gap-2 md:gap-4">
         <button className="text-[10px] sm:text-sm md:text-base lg:text-xl text-center font-semibold border border-white text-white bg-transparent rounded-2xl hover:bg-white hover:text-black whitespace-nowrap px-6 py-2 md:px-10 md:py-2 lg:px-14 lg:py-4 md:w-full lg:w-full">
-          {hero.buttons.buttonLogIn.label}
+          {buttonLogInLabel}
         </button>
         <button className="text-[10px] sm:text-sm md:text-base lg:text-xl text-center font-semibold bg-white text-darkblue rounded-2xl hover:bg-darkblue hover:text-white whitespace-nowrap px-4 py-2 md:px-8 md:py-2 lg:px-10 lg:py-4 md:w-full lg:w-full">
-          {hero.buttons.buttonTryForFree.label}
+          {buttonTryForFreeLabel}
         </button>
       </div> */}
     </nav>
