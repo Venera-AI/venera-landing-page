980fed0d3ab5166f7f17b668d9ceb2bc10bf63a4
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2025-04-04 17:50:33 -0400

    Privacy and Terms pages (#29)

---
+++ src/app/[locale]/terms/page.tsx
@@ -0,0 +1,9 @@
+import { react as Terms } from "./terms.md";
+
+export default function Page() {
+  return (
+    <main className="max-w-7xl py-10 lg:py-20 px-4 mx-auto prose">
+      <Terms />
+    </main>
+  );
+}
