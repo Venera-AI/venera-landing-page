6c8c10e1a870adafddb7aecad208b04e7a240973
Huong Pham <hazel@viai.io>
2025-04-04 12:36:01 -0400

    finalize styling for all screens

--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,26 +1,27 @@
 import Image from "next/image";
 import homeData from "@/content/home.json";
+
 export default function NavBar() {
   const { hero } = homeData;
   return (
-    <nav className="flex justify-between items-center w-full py-3 px-3 shadow-md bg-gradient-to-r from-[#83a3cb] to-[#5162b2] md:px-6 lg:p-3 lg:px-8">
+    <nav className="flex justify-between items-center w-full py-3 px-3 md:px-6 lg:p-3 lg:px-8 shadow-md bg-gradient-to-r from-[#83a3cb] to-[#5162b2]">
       <div className="flex justify-center md:justify-start">
         <Image
           src={hero.logos.logoNavBar}
           alt="Venera Logo"
           width={200}
           height={80}
-          className="max-w-[150px] sm:min-w-[200px]"
+          className="w-full max-w-full h-auto sm:min-w-[200px]"
         />
       </div>
-      <div className="flex justify-end gap-2 md:gap-4">
-        <button className="text-center border border-white text-white font-semibold bg-transparent rounded-2xl hover:bg-white hover:text-black text-[10px] whitespace-nowrap px-6 py-2 sm:text-sm md:w-full md:text-base md:px-10 md:py-2 lg:w-full lg:text-xl lg:px-14 lg:py-4">
+      {/* <div className="flex justify-end gap-2 md:gap-4">
+        <button className="text-[10px] sm:text-sm md:text-base lg:text-xl text-center font-semibold border border-white text-white bg-transparent rounded-2xl hover:bg-white hover:text-black whitespace-nowrap px-6 py-2 md:px-10 md:py-2 lg:px-14 lg:py-4 md:w-full lg:w-full">
           {hero.buttons.buttonLogIn.label}
         </button>
-        <button className=" text-center text-darkblue bg-white font-semibold rounded-2xl hover:bg-darkblue hover:text-white px-4 py-2 whitespace-nowrap text-[10px] sm:text-sm md:w-full md:text-base md:px-8 md:py-2 lg:w-full lg:text-xl lg:px-10 lg:py-4 ">
+        <button className="text-[10px] sm:text-sm md:text-base lg:text-xl text-center font-semibold bg-white text-darkblue rounded-2xl hover:bg-darkblue hover:text-white whitespace-nowrap px-4 py-2 md:px-8 md:py-2 lg:px-10 lg:py-4 md:w-full lg:w-full">
           {hero.buttons.buttonTryForFree.label}
         </button>
-      </div>
+      </div> */}
     </nav>
   );
 }
--- src/app/sections/hero-section.tsx
+++ src/app/sections/hero-section.tsx
@@ -8,68 +8,63 @@ export default function HeroSection() {
   return (
     <div className="bg-gradient-to-r from-[#9ec5f7] to-darkblue">
       <NavBar />
-      <div className="flex flex-col gap-4 py-10 md:justify-between lg:flex-row lg:max-w-7xl lg:pb-5">
+      <div className="flex flex-col lg:flex-row gap-5 py-10 lg:py-15 md:justify-between mx-auto lg:max-w-7xl">
         {/* Left Section */}
-        <div className="w-full p-3 text-center md:p-10 lg:w-[60%] lg:p-0 lg:pl-20">
+        <div className="w-full lg:w-[60%] p-3 md:p-10 lg:p-0 lg:pl-20 text-center">
           <div className="relative">
-            <div className="z-10 pt-40 lg:pt-20">
-              <div className="mb-7 text-3xl font-bold text-white text-center md:mt-30 md:text-5xl lg:text-left lg:text-7xl">
-                <h1>{hero.headline}</h1>
+            <div className="w-full sm:top-[-30] md:top-0 md:h-auto md:pl-0 lg:top-0 lg:h-auto top-0 left-1/2">
+              <div className="relative mx-auto aspect-square w-full max-w-[500px]">
+                <Image
+                  src={hero.images.textEffect}
+                  alt="Left Section Effect"
+                  fill
+                  className="object-cover"
+                />
               </div>
-              <div className="text-xl text-white text-center md:text-2xl lg:text-left lg:text-5xl">
-                <span className="inline-block">
-                  <h3>{hero.subheadline}</h3>
-                </span>
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full lg:pt-20 z-10 transform">
+                <div className="text-white text-center lg:text-left text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold mb-7">
+                  <h1>{hero.headline}</h1>
+                </div>
+                <div className="text-white text-center lg:text-left text-xl md:text-2xl lg:text-5xl">
+                  <span className="inline-block">
+                    <h3>{hero.subheadline}</h3>
+                  </span>
+                </div>
               </div>
-            </div>
-            <div className="absolute top-0 left-0 w-full aspect-square lg:w-[80%]">
-              <Image
-                src={hero.images.leftEffect}
-                alt="Left Section Effect"
-                fill
-                className="object-cover"
-              />
             </div>
           </div>
         </div>
 
         {/* Right Section */}
-        <div className="relative flex justify-center w-full pt-40 sm:pt-80 md:pt-70 md:py-20 lg:w-[40%] lg:py-0">
-          <div className="absolute w-[300px] h-[350px] md:top-[40%] md:left-[18%] md:w-[550px] md:h-[400px] lg:left-[5%] lg:top-[20px] lg:w-[90%] lg:h-[550px]">
-            <Image
-              src={hero.images.rightPhoneEffect}
-              alt="Phone Effect"
-              className="object-contain"
-              fill
-            />
-          </div>
-          <div className="relative z-10 flex items-center justify-center md:mb-5 md:mt-30 lg:top-[-100px]">
+        <div className="lg:relative lg:flex w-full lg:w-[40%] md:py-20 md:mt-0 lg:py-0">
+          <div className="relative flex flex-col mx-auto items-center justify-center">
+            <div className="lg:absolute lg:z-40 w-full lg:top-[70%] lg:left-[0] lg:translate-x-[-60%]">
+              <button className="relative block mx-auto items-center max-w-[150px] sm:max-w-[180px] lg:max-w-[200px] w-full mb-5 rounded-lg cursor-pointer">
+                <Image
+                  src={hero.buttons.buttonAppStore.img}
+                  alt="App Store button"
+                  className="object-contain w-full h-auto"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                />
+              </button>
+            </div>
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full lg:w-[150%] h-full z-10 transform">
+              <Image
+                src={hero.images.phoneEffect}
+                alt="Phone Effect"
+                className="object-contain"
+                fill
+              />
+            </div>
             <Image
               alt="Phone MockUp"
               src={hero.images.mockUpPhone}
-              className="w-[180px] md:w-[280px] lg:w-[300px]"
-              width={250}
-              height={250}
+              className="relative max-w-[200px] sm:max-w-[240px] md:max-w-[280px] lg:max-w-[300px] w-full z-30"
+              width={0}
+              height={0}
             />
-          </div>
-          <div className="absolute z-20 transform -translate-x-1/2 left-1/3 top-[100] md:left-[40%] md:top-[300] lg:top-[58%] lg:left-[-5%]">
-            <button className="absolute flex flex-row gap-2 items-center bg-white text-black rounded-lg hover:bg-gray-200 pl-1 pr-7 py-1 cursor-pointer md:pl-2 md:pr-13 md:py-1 lg:pl-6 lg:pr-18 lg:py-2">
-              <Image
-                src={hero.logos.appleLogo}
-                alt="button App store"
-                className="mr-1 w-1/4 md:w-6 md:h-6 lg:w-8 lg:h-8"
-                width={4}
-                height={4}
-              />
-              <div className="flex flex-col w-2/3 whitespace-nowrap mr-2">
-                <span className="text-xs md:text-base">
-                  {hero.buttons.buttonAppStore.label}
-                </span>
-                <div className="text-xs font-bold whitespace-nowrap md:text-xl">
-                  {hero.buttons.buttonAppStore.app}
-                </div>
-              </div>
-            </button>
           </div>
         </div>
       </div>
