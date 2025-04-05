13fe109fcc3ac64d5cd0afa1fe8bc6ccbb475337
Hung Nguyen <nthungdev@gmail.com>
2025-04-05 16:08:37 -0400

    refactor features section

--- src/app/[locale]/sections/features-section.tsx
+++ src/app/[locale]/sections/features-section.tsx
@@ -28,7 +28,7 @@ export default function FeaturesSection() {
         <SlideIn
           duration={1}
           as="h2"
-          className="mb-12 xl:mb-20 text-[42px] text-center lg:text-left leading-tight"
+          className="mb-12 xl:mb-20 text-[56px] text-center lg:text-left leading-tight"
         >
           {separatedHeadlineChunks.map((chunk, index) =>
             chunk === "," ? (
@@ -44,40 +44,40 @@ export default function FeaturesSection() {
 
         <div className="grid gap-8 xl:gap-16">
           {itemKeys.map((key, index) => (
-            <div key={key} className="relative">
-              <SlideIn
-                duration={0.5}
-                delay={0.5}
+            <SlideIn
+              key={key}
+              duration={0.5}
+              className="relative px-8 lg:px-11 py-10 xl:pr-28 rounded-4xl flex flex-col lg:flex-row items-center gap-12 xl:gap-14"
+            >
+              <div
                 className={clsx(
-                  "absolute inset-0 rounded-4xl",
+                  "absolute -z-10 inset-0 rounded-4xl",
                   gradientBackgrounds[index],
                 )}
               />
-              <div className="relative px-8 lg:px-11 py-10 xl:pr-40 rounded-4xl flex flex-col lg:flex-row items-center gap-12 xl:gap-14">
-                <div>
-                  <SlideIn
-                    duration={0.5}
-                    delay={0.2}
-                    className="text-[32px] text-center lg:text-start leading-12"
-                  >
-                    {t(`items.${key}.headline`)}
-                  </SlideIn>
-                  <p className="font-light text-2xl mt-4 lg:w-[80%] text-center lg:text-start leading-normal">
-                    {t(`items.${key}.body`)}
-                  </p>
-                </div>
-
-                <SlideIn duration={0.5} className="w-full">
-                  <Image
-                    src={t(`items.${key}.image`)}
-                    alt=""
-                    width={300}
-                    height={250}
-                    className="w-full max-w-[400px] rounded-lg"
-                  />
+              <div className="h-full flex flex-col justify-center">
+                <SlideIn
+                  duration={0.5}
+                  delay={0.5}
+                  className="text-[42px] text-center lg:text-start leading-12"
+                >
+                  {t(`items.${key}.headline`)}
+                </SlideIn>
+                <SlideIn
+                  delay={0.6}
+                  className="font-light text-[32px] mt-4 lg:w-[80%] text-center lg:text-start leading-normal"
+                >
+                  {t(`items.${key}.body`)}
                 </SlideIn>
               </div>
-            </div>
+              <Image
+                src={t(`items.${key}.image`)}
+                alt=""
+                width={0}
+                height={0}
+                className="w-full max-w-[400px] rounded-lg"
+              />
+            </SlideIn>
           ))}
         </div>
       </div>
