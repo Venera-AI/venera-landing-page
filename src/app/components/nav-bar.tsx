c882160fcb734a736c9837e85c887fd03925ec13
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-04-05 17:12:19 +0700

    chore: move locale switcher to hero header

--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,3 +1,4 @@
+import LocaleSwitcher from "@/app/components/locale-switcher";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
@@ -15,6 +16,9 @@ export default function NavBar() {
           className="w-full max-w-full h-auto sm:min-w-[200px]"
         />
       </div>
+
+      {/* TODO: Move this into the container above when we decide to uncomments the two buttons below */}
+      <LocaleSwitcher />
 
       {/* <div className="flex justify-end gap-2 md:gap-4">
         <button className="text-[10px] sm:text-sm md:text-base lg:text-xl text-center font-semibold border border-white text-white bg-transparent rounded-2xl hover:bg-white hover:text-black whitespace-nowrap px-6 py-2 md:px-10 md:py-2 lg:px-14 lg:py-4 md:w-full lg:w-full">
