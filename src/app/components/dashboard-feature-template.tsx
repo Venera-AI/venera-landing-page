de220e3feaaa986c21fb393f91e7504e45a0d88c
Hung Nguyen <nthungdev@gmail.com>
2025-04-05 03:53:54 -0400

    refactor dashboard template

--- src/app/components/dashboard-feature-template.tsx
+++ src/app/components/dashboard-feature-template.tsx
@@ -15,34 +15,28 @@ export default function DashboardFeatureTemplate({
   circleColor,
 }: DashboardFeatureTemplateProps) {
   return (
-    <section className="bg-white mb-24">
-      <div className="flex flex-col lg:flex-row gap-8 items-center max-w-7xl py-16 px-8 m-auto relative">
-        <div className="flex flex-col flex-1">
+    <section className="bg-white overflow-x-hidden overflow-y-visible py-16 md:py-24 px-8">
+      <div className="flex flex-col lg:flex-row gap-8 md:gap-10 lg:justify-between items-center max-w-7xl m-auto relative">
+        <div className="flex-1">
           <h2 className="text-4xl font-medium text-center lg:text-start leading-12">
             {headline}
           </h2>
-          <p className="text-xl mt-8 lg:w-[80%] text-center lg:text-start leading-8">
+          <p className="text-xl mt-8 lg:mt-10 lg:w-[80%] text-center lg:text-start leading-8">
             {body}
           </p>
         </div>
 
-        <div className="relative flex justify-end items-center flex-1 mt-12 lg:mt-0">
+        <div className="relative">
           <div
-            className="absolute z-0 rounded-full w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[450px] lg:h-[450px]"
-            style={{
-              background: circleColor,
-              top: "50%",
-              left: "calc(100% - 66%)",
-              transform: "translateY(-50%)",
-            }}
+            className="hidden lg:block absolute z-0 rounded-full aspect-square h-[120%] lg:h-[140%] top-1/2 -translate-y-1/2 left-[30%]"
+            style={{ background: circleColor }}
           ></div>
           <Image
             src={image}
             alt={alt}
-            width={565}
-            height={511}
-            sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 33vw"
-            className="relative z-10 max-w-[200px] md:max-w-[300px] lg:max-w-[450px]"
+            width={0}
+            height={0}
+            className="relative z-10 w-full lg:max-w-[500px] xl:max-w-[550px]"
           />
         </div>
       </div>
