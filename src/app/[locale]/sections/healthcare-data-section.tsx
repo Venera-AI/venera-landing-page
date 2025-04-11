6367056fcf981f5920bbabeb2957fa52eaf48757
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2025-04-11 11:28:21 +0700

    style: remove delay from healthcare section

--- src/app/[locale]/sections/healthcare-data-section.tsx
+++ src/app/[locale]/sections/healthcare-data-section.tsx
@@ -30,7 +30,7 @@ export default function HealthcareDataSection() {
             <SlideIn
               as="li"
               direction="right"
-              delay={0.2 + 0.2 * index}
+              delay={0.2 * index}
               key={index}
               className={clsx(
                 "h-full p-5 md:p-4 lg:p-6 space-y-4 lg:space-y-6 rounded-3xl overflow-hidden",
