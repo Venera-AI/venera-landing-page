a5d3a4560127cd6098b3ef7b06ee12052895320e
Hung Nguyen <nthungdev@gmail.com>
2025-04-24 23:15:43 -0400

    define types for env var

--- global.d.ts
+++ global.d.ts
@@ -6,3 +6,19 @@ declare module "*.md" {
   const body: string;
   export { react, meta, body, html, attributes };
 }
+
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_FIREBASE_API_KEY?: string;
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN?: string;
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID?: string;
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET?: string;
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID?: string;
+    NEXT_PUBLIC_FIREBASE_APP_ID?: string;
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID?: string;
+    NEXT_PUBLIC_USE_FIREBASE_EMULATOR?: string;
+    NEXT_PUBLIC_IP_INFO_TOKEN?: string;
+    NEXT_PUBLIC_BASE_URL?: string;
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY?: string;
+  }
+}
