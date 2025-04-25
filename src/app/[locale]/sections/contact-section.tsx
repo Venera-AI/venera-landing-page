37fb660823c134a0c24d085f72905d4cbe714f28
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2025-04-25 16:02:04 -0400

    VBW-26 limit firestore db access (#40)

--- src/app/[locale]/sections/contact-section.tsx
+++ src/app/[locale]/sections/contact-section.tsx
@@ -1,10 +1,14 @@
 "use client";
+
 import React, { useState } from "react";
-import { db } from "@/services/firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { useTranslations } from "next-intl";
 import { UserLocation } from "@/types";
 import SlideIn from "@/components/slide-in";
+import clsx from "clsx";
+
+// in milliseconds
+const STATUS_TIMEOUT = 5000;
 
 export default function ContactSection() {
   const t = useTranslations("homePage.contact");
@@ -19,6 +23,7 @@ export default function ContactSection() {
     success: null,
   });
   const [location, setLocation] = useState<UserLocation | null>(null);
+  const [submitting, setSubmitting] = useState(false);
 
   const MESSAGES = {
     REQUIRED_FIELDS: "All fields are required!",
@@ -47,6 +52,14 @@ export default function ContactSection() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    setSubmitting(true);
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const hannibotField = formData.get("nickname");
+    if (hannibotField) {
+      // If the honeypot field is filled, it's likely a bot
+      return;
+    }
+
     const trimmedName = name.trim();
     const trimmedEmail = email.trim();
     const trimmedMessage = message.trim();
@@ -56,12 +69,18 @@ export default function ContactSection() {
         error: MESSAGES.REQUIRED_FIELDS,
         success: null,
       });
-      setTimeout(() => setStatus({ error: null, success: null }), 5000);
+      setTimeout(
+        () => setStatus({ error: null, success: null }),
+        STATUS_TIMEOUT,
+      );
+      setSubmitting(false);
       return;
     }
 
     try {
-      // If location is null, it's ok to continue without it
+      // import firebase here to avoid server-side rendering issues
+      const { db } = await import("@/services/firebase");
+      // It's ok to continue without location
       const currentLocation = location || (await fetchLocation());
       const messagesRef = collection(db, "messages");
       await addDoc(messagesRef, {
@@ -75,7 +94,10 @@ export default function ContactSection() {
         error: null,
         success: MESSAGES.SUCCESS,
       });
-      setTimeout(() => setStatus({ error: null, success: null }), 5000);
+      setTimeout(
+        () => setStatus({ error: null, success: null }),
+        STATUS_TIMEOUT,
+      );
       setName("");
       setEmail("");
       setMessage("");
@@ -85,6 +107,8 @@ export default function ContactSection() {
         error: MESSAGES.ERROR,
         success: null,
       });
+    } finally {
+      setSubmitting(false);
     }
   };
 
@@ -125,8 +149,21 @@ export default function ContactSection() {
           placeholder={t("messageInput")}
           rows={3}
         />
+        {/* Honeypot field */}
+        <input
+          className="hidden"
+          type="text"
+          name="nickname"
+          autoComplete="off"
+        />
         <div className="lg:justify-end flex justify-center mt-3">
-          <button className="cursor-pointer lg:p-3 lg:w-1/4 lg:text-xl w-1/2 px-6 py-2 bg-gradient-to-r from-[#2d44ad] to-[#9ec5f7] text-white rounded-xl font-bold">
+          <button
+            className={clsx(
+              "lg:p-3 lg:w-1/4 lg:text-xl w-1/2 px-6 py-2 bg-gradient-to-r from-[#2d44ad] to-[#9ec5f7] text-white rounded-xl font-bold",
+              submitting ? "opacity-50 cursor-not-allowed" : "cursor-pointer",
+            )}
+            disabled={submitting}
+          >
             {t("actionButton.label")}
           </button>
         </div>
