52a0be6669e189576d6623430a6e7476e317c3bb
Huong Pham <hazel@viai.io>
2025-06-11 23:19:52 +0700

    fix gap between sponsorship logos

--- src/app/[locale]/sections/sponsorship-section.tsx
+++ src/app/[locale]/sections/sponsorship-section.tsx
@@ -9,18 +9,18 @@ export default function SponsorshipSection() {
   ) as readonly (keyof typeof sponsorshipMessages.logos)[];
   return (
     <section>
-      <div className="flex flex-col items-center justify-center py-16 px-8 md:p-16 max-w-7xl m-auto">
+      <div className="flex flex-col items-center justify-center py-16 px-8 md:p-16 max-w-7xl m-auto gap-3">
         <SlideIn
           as="h2"
           className="text-4xl font-medium text-center text-navyblue"
         >
           {t("headline")}
         </SlideIn>
-        <ul className="flex flex-wrap justify-center gap-7">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-7">
           {imageKeys.map((key) => (
             <li
               key={key}
-              className="relative size-50 max-w-[250px] md:max-w-none lg:size-70"
+              className="relative size-50 max-w-[230px] md:max-w-none lg:size-65"
             >
               <span className="sr-only">{`${sponsorshipMessages.logos[key].alt} sponsor`}</span>
               <Image
