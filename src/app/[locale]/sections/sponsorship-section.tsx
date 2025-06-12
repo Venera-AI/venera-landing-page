0094e648d764f623dd52f143ad128690ed7a65f4
huongphamviai <hazel@viai.io>
2025-06-13 02:41:58 +0700

    Vbw 38 add sponsorship section (#45)
    Co-authored-by: Hung Nguyen <31390562+nthungdev@users.noreply.github.com>

--- src/app/[locale]/sections/sponsorship-section.tsx
+++ src/app/[locale]/sections/sponsorship-section.tsx
@@ -9,19 +9,16 @@ export default function SponsorshipSection() {
   ) as readonly (keyof typeof sponsorshipMessages.logos)[];
   return (
     <section>
-      <div className="flex flex-col items-center justify-center py-16 px-8 md:p-16 max-w-7xl m-auto">
+      <div className="flex flex-col items-center justify-center py-16 px-8 md:p-16 max-w-7xl m-auto gap-3">
         <SlideIn
           as="h2"
           className="text-4xl font-medium text-center text-navyblue"
         >
           {t("headline")}
         </SlideIn>
-        <ul className="flex flex-wrap justify-center gap-7">
+        <ul className="flex flex-wrap flex-row justify-center gap-x-8">
           {imageKeys.map((key) => (
-            <li
-              key={key}
-              className="relative size-50 max-w-[250px] md:max-w-none lg:size-70"
-            >
+            <li key={key} className="relative w-50 h-35 md:h-40">
               <span className="sr-only">{`${sponsorshipMessages.logos[key].alt} sponsor`}</span>
               <Image
                 src={sponsorshipMessages.logos[key].image}
